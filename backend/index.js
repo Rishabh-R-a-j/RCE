4e71e8e228e0ba80083056850265e9c8ffc62fcb
unknown <riddhiagrawal0609@gmail.com>
2024-06-02 12:27:07 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -10,45 +10,6 @@ require('dotenv').config();
 
 console.log(process.env.JDoodle_ClientID)
 
-// app.post("/compile", (req, res) => {
-//     //getting the required data from the request
-//     let code = req.body.code;
-//     let language = req.body.language;
-//     let input = req.body.input;
-
-//     // console.log(code);
-//     // console.log(language);
-//     // console.log(input);
-//     if (language === "python") {
-//         language = "py"
-//     }
-
-//     let data = ({
-//         "code": code,
-//         "language": language,
-//         "input": input
-//     });
-//     let config = {
-//         method: 'post',
-//         url: 'https://codexweb.netlify.app/.netlify/functions/enforceCode',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         data: data
-//     };
-//     //calling the code compilation API
-//     Axios(config)
-//         .then((response) => {
-//             console.log("Sent");
-//             res.send(response.data)
-//            // console.log(response.data)
-//         }).catch((error) => {
-//         //     console.log("Error");
-//         console.log(error);
-//         });
-// })
-// sk-proj-ceLrFzu9GLM74MvWGXgOT3BlbkFJidlgFNfwzFb2VL3Ik9nR
-// { apiKey: "sk-proj-ceLrFzu9GLM74MvWGXgOT3BlbkFJidlgFNfwzFb2VL3Ik9nR" }
 const JDoodle_ClientID = process.env.JDoodle_ClientID.toString();
 const JDoodle_ClientSecret = process.env.JDoodle_ClientSecret.toString();
 console.log(JDoodle_ClientID,JDoodle_ClientSecret)
